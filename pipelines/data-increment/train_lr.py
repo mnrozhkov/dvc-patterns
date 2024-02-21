642b7a1a7ddcf0a2b7718c80fc32ced875f45cf8
Mikhail <mnrozhkov@gmail.com>
2024-02-21 19:51:27 +0100

    New experiment

--- pipelines/data-increment/train_lr.py
+++ pipelines/data-increment/train_lr.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import joblib
+import random
 from sklearn.linear_model import LogisticRegression 
 import time
+
+from dvclive import Live
+from dvc.api import params_show
+
 
 def train_model_LR():
 
@@ -18,6 +23,18 @@ def train_model_LR():
     # Train the model
     model = LogisticRegression()
     model.fit(X, y)
+
+    with Live(save_dvc_exp=True) as live:
+        time.sleep(3)
+        epochs = 10
+        
+        for i in range(10):
+            print(f"training on epoch: {i}")
+            live.log_metric("accuracy", i + random.random())
+            live.log_metric("f1", epochs - (i + random.random()))
+            live.next_step()
+
+            time.sleep(epochs)
 
     # Save the trained model
     joblib.dump(model, 'models/model_LR.pkl')
